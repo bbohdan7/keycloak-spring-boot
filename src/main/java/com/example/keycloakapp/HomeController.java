b8adf7e3acee39f4f61d91b18cd690615cf7f283
Bogdan <zbohdan7@gmail.com>
2021-06-08 00:08:28 +0300

    Initial

---
+++ src/main/java/com/example/keycloakapp/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.keycloakapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+
+
+    @GetMapping("/customers")
+    public String index(Principal principal, Model model) {
+        model.addAttribute("user", "John");
+        //model.addAttribute("username", principal.getName());
+        return "home";
+    }
+
+}
